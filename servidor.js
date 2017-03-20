87759cc3d8c07cd11849b2195451267dffd11be2
Luis Garijo <luisgarijog@gmail.com>
2017-03-20 12:03:02 +0100

    funciona correctamente

--- servidor.js
+++ servidor.js
@@ -6,9 +6,9 @@ var io = require('socket.io').listen(server);
 var bodyParser = require('body-parser');
 
 var productos = [
-{id:'producto1', nombre:'GENUINO CERO', MICRO:" ATSAMD21G18", TF:"3.3V", UART:"2 (Nativo y Programación)", FLASH:"256 KB", RAM:"32 KB", V:"48 MHz"},
-{id:'producto2', nombre:'ARDUINO UNO', MICRO:"ATmega328P", TF:"5V", UART:"14 (6 proporcionan salida PWM)", FLASH:"32 KB", RAM:"2 KB", V:"16 MHz"},
-{id:'producto3', nombre:'GENUINO 101', MICRO:"Intel Curie", TF:"3.3V", UART:"14 I/O (4 proporcionan salida PWM)", FLASH:"196 kB", RAM:"24 kB", V:"32 MHz"}
+{id:'producto1', nombre:'GENUINO CERO', precio:'120', MICRO:" ATSAMD21G18", TF:"3.3V", UART:"2 (Nativo y Programación)", FLASH:"256 KB", RAM:"32 KB", V:"48 MHz"},
+{id:'producto2', nombre:'ARDUINO UNO', precio:'80', MICRO:"ATmega328P", TF:"5V", UART:"14 (6 proporcionan salida PWM)", FLASH:"32 KB", RAM:"2 KB", V:"16 MHz"},
+{id:'producto3', nombre:'GENUINO 101', precio:'95', MICRO:"Intel Curie", TF:"3.3V", UART:"14 I/O (4 proporcionan salida PWM)", FLASH:"196 kB", RAM:"24 kB", V:"32 MHz"}
 ];
 
 app.set('view engine', 'jade');
